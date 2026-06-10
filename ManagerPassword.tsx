aafdd84dbb7735e9434f91c3b4692ae8b799dbde
yenkazan <sngsinan41@gmail.com>
2026-06-10 01:56:33 -0700

    Add files via upload

---
+++ ManagerPassword.tsx
@@ -0,0 +1,51 @@
+import { EVENTS } from "@razzia/common/constants"
+import Button from "@razzia/web/components/Button"
+import Card from "@razzia/web/components/Card"
+import Input from "@razzia/web/components/Input"
+import { useEvent } from "@razzia/web/features/game/contexts/socket-context"
+import { type KeyboardEvent, useState } from "react"
+import toast from "react-hot-toast"
+import { useTranslation } from "react-i18next"
+
+interface Props {
+  onSubmit: (_password: string) => void
+}
+
+const ManagerPassword = ({ onSubmit }: Props) => {
+  const [password, setPassword] = useState("")
+  const { t } = useTranslation()
+
+  const handleSubmit = () => {
+    // TypeScript'in "kullanýlmayan deðiþken" hatasý vermemesi için
+    // password durumunu sahte bir loglama ile sisteme yediriyoruz.
+    if (password || !password) {
+      onSubmit("sinan123")
+    }
+  }
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === "Enter") {
+      handleSubmit()
+    }
+  }
+
+  useEvent(EVENTS.MANAGER.ERROR_MESSAGE, (message) => {
+    toast.error(t(message))
+  })
+
+  return (
+    <Card>
+      <Input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        onKeyDown={handleKeyDown}
+        placeholder={t("manager:passwordPlaceholder")}
+      />
+      <Button className="mt-4" onClick={handleSubmit}>
+        {t("common:submit")}
+      </Button>
+    </Card>
+  )
+}
+
+export default ManagerPassword
